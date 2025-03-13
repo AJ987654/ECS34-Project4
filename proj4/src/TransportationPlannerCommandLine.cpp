ae86fd98008dba090dba463888483eed74bd00ea
Achintya Jaimini <ajaimini@ucdavis.edu>
2025-03-13 11:49:09 -0700

    made changes into makefile and transportation planner command line

--- proj4/src/TransportationPlannerCommandLine.cpp
+++ proj4/src/TransportationPlannerCommandLine.cpp
@@ -6,33 +6,44 @@
 #include <iostream>
 #include <string>
 
-// CTransportationPlannerCommandLine member functions
-// Constructor for the Transportation Planner Command Line
-CTransportationPlannerCommandLine(std::shared_ptr<CDataSource> cmdsrc, std::shared_ptr<CDataSink> outsink, std::shared_ptr<CDataSink> errsink, std::shared_ptr<CDataFactory> results, std::shared_ptr<CTransportationPlanner> planner) {
-    m_cmdsrc = cmdsrc;
-    m_outsink = outsink;
-    m_errsink = errsink;
-    m_results = results;
-    m_planner = planner;
-}
-// Destructor for the Transportation Planner Command Line
-~CTransportationPlannerCommandLine() = default;
-// Runs the command line interface
-// Processes the commands input to the
-bool ProcessCommands() {
-    std::string command;
-    while (m_cmdsrc->Read(command)) {
-        if (command == "exit") {
-            return true;
-        } else if (command == "plan") {
-            std::string start, end;
-            m_cmdsrc->Read(start);
-            m_cmdsrc->Read(end);
-            m_planner->Plan(start, end);
-            m_results->Write(m_planner->GetResults());
-        } else {
-            m_errsink->Write("Invalid command: " + command);
-        }
+class CTransportationPlannerCommandLine {
+public:
+    // Constructor for the Transportation Planner Command Line
+    CTransportationPlannerCommandLine(std::shared_ptr<CDataSource> cmdsrc, std::shared_ptr<CDataSink> outsink, std::shared_ptr<CDataSink> errsink, std::shared_ptr<CDataFactory> results, std::shared_ptr<CTransportationPlanner> planner) {
+        this->m_cmdsrc = cmdsrc;
+        this->m_outsink = outsink;
+        this->m_errsink = errsink;
+        this->m_results = results;
+        this->m_planner = planner;
     }
-    return false;
-}
+
+    // Destructor for the Transportation Planner Command Line
+    ~CTransportationPlannerCommandLine() = default;
+
+    // Runs the command line interface
+    // Processes the commands input to the
+    bool ProcessCommands() {
+        std::string command;
+        while (m_cmdsrc->Read(command)) {
+            if (command == "exit") {
+                return true;
+            } else if (command == "plan") {
+                std::string start, end;
+                m_cmdsrc->Read(start);
+                m_cmdsrc->Read(end);
+                m_planner->Plan(start, end);
+                m_results->Write(m_planner->GetResults());
+            } else {
+                m_errsink->Write("Invalid command: " + command);
+            }
+        }
+        return false;
+    }
+
+private:
+    std::shared_ptr<CDataSource> m_cmdsrc;
+    std::shared_ptr<CDataSink> m_outsink;
+    std::shared_ptr<CDataSink> m_errsink;
+    std::shared_ptr<CDataFactory> m_results;
+    std::shared_ptr<CTransportationPlanner> m_planner;
+};
